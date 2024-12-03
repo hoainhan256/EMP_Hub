ef7b4fbb82e0b1b2df38c5246844b2e9338a0a7b
hoainhan256 <145365603+hoainhan256@users.noreply.github.com>
2024-12-03 20:33:47 +0700

    connect to mysql database

--- EMPSites/EMPSites/settings.py
+++ EMPSites/EMPSites/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'EMPSites.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "emp",
+        "USER": "root",
+        "PASSWORD": "",
+        "HOST": "127.0.0.1",
+        "PORT": "3309",
     }
 }
 
