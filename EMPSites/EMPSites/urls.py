8dc8235a94ddecdb4649182116eb6f347a4c9264
Apudidon <Apudidon@gmail.com>
2025-01-10 16:36:38 +0700

    Start EventCreate

--- EMPSites/EMPSites/urls.py
+++ EMPSites/EMPSites/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('log_sign.urls')),
+    path('', include('EventCreate.urls')),
     
     path('admin/', admin.site.urls),
 
---
+++ EMPSites/EventCreate/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EventcreateConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'EventCreate'
