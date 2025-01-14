ad57f9cdecbc16a6e80a214a75024340103d1247
hoainhan256 <145365603+hoainhan256@users.noreply.github.com>
2025-01-15 05:29:04 +0700

    Home Page part1

--- EMPSites/EMPSites/urls.py
+++ EMPSites/EMPSites/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
 urlpatterns = [
+   
     path('', include('log_sign.urls')),
     path('', include('EventCreate.urls')),
     
     path('admin/', admin.site.urls),
-
+    path('', include('home.urls')),
 ]
