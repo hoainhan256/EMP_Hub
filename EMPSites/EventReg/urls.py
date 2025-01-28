6d2fdcacdc308368e7f0af823d4ac7c2caf24f16
Giang-Nguyen-2005 <nguyengiang2005a@gmail.com>
2025-01-28 16:50:12 +0700

    Send email to event registrants

--- EMPSites/EventReg/urls.py
+++ EMPSites/EventReg/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.form_reg, name=""),
-   
+    path('', views.form_reg, name="form_reg"),
+    path('submit-registration/', views.submit_registration, name="submit_registration"),
+    
 ]
