5bd3ac77c401f0ce8e8ba248c19b1be5401c4254
hoainhan256 <145365603+hoainhan256@users.noreply.github.com>
2025-02-02 19:44:19 +0700

    login complete

--- EMPSites/log_sign/urls.py
+++ EMPSites/log_sign/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('login/', views.login, name='login'),
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutPage, name='logout'),
 ]
