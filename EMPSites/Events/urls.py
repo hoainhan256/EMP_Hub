6d49eb42b1e1f33f1d1779dc790616acf3ccd886
hieumous <hieutv0599@ut.edu.vn>
2025-02-14 11:07:40 +0700

    update Edit and Dlete Create category

--- EMPSites/Events/urls.py
+++ EMPSites/Events/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import ( 
     EventCategoryCreateView,
+    EventCategoryUpdateView,
+    EventCategoryDeleteView,
     category_list 
     )
 
@@ -9,4 +12,8 @@ from .views import (
 urlpatterns = [
 path('create-category/', EventCategoryCreateView.as_view(), name='create_event_category'),
 path('category-list/', category_list, name='event_category_list'),
+path('category/<int:pk>/edit/', EventCategoryUpdateView.as_view(), name='edit_event_category'),
+path('category/<int:pk>/delete/', EventCategoryDeleteView.as_view(), name='delete_event_category'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- EMPSites/Events/views.py
+++ EMPSites/Events/views.py
@@ -1,7 +1,10 @@
 from django.views.generic import (
     CreateView,
+    UpdateView,
+    DeleteView,
 )
-
+from django.urls import reverse_lazy
+from django.contrib import messages
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
@@ -26,3 +29,24 @@ def category_list(request):
     return render(request, 'Events/event_category.html', {'event_category': event_category})
 
 
+class EventCategoryUpdateView(LoginRequiredMixin, UpdateView):
+    login_url = 'login'
+    model = EventCategory
+    fields = ['name', 'code', 'image', 'priority', 'status']
+    template_name = 'Events/edit_event_category.html'
+
+def form_valid(self, form):
+        messages.success(self.request, "Category updated successfully!")
+        return super().form_valid(form)
+def get_success_url(self):
+        return reverse_lazy('event_category_list')
+
+class EventCategoryDeleteView(LoginRequiredMixin, DeleteView):
+    model = EventCategory
+    template_name = 'Events/delete_event_category.html'  # Trang xác nhận xóa
+    success_url = reverse_lazy('event_category_list')  # Điều hướng sau khi xóa
+    success_message = "Category deleted successfully!"
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super().delete(request, *args, **kwargs)
